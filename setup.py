cf56cfe7dd3dfe45a3e407302e036a6c5f41835c
Victor3637 <136455069+Victor3637@users.noreply.github.com>
2023-08-07 10:06:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
+  name = 'personal assistant',
+  version = '0.2',
+  description = 'особистий асистент',
+  url = 'https://github.com/Pelmenoff/dedalus_project/tree/Victor3637-patch-1',
+  authors='Pelmenoff' 'Nikita-devel' 'Victor3637',
+  author_email='victorpiznak@gmail.com' 'brseven90@gmail.com' 'likhachiovvl@gmail.com' '',
+  license='MIT',
+  packages=find_namespace_packages(),
+  entry_points = ()
 
-
-
-
-
-
-  
 )
