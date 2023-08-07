5538712736fd67107b46c5dece20811ffb80406a
Victor3637 <136455069+Victor3637@users.noreply.github.com>
2023-08-07 10:38:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   description = 'особистий асистент',
   url = 'https://github.com/Pelmenoff/dedalus_project/tree/Victor3637-patch-1',
   authors='Pelmenoff' 'Nikita-devel' 'Victor3637',
-  author_email='victorpiznak@gmail.com' 'brseven90@gmail.com' 'likhachiovvl@gmail.com' '',
+  author_email='victorpiznak@gmail.com' 'brseven90@gmail.com' 'likhachiovvl@gmail.com' 'Titanfall@ukr.net',
   license='MIT',
   packages=find_namespace_packages(),
   entry_points = ()
