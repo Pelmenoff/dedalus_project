f2c67f525dd3e921bdbf00b5248885d1e5e7d3d8
Victor3637 <136455069+Victor3637@users.noreply.github.com>
2023-08-08 20:56:24 +0300

    Create setup.py

---
+++ DEDALUS/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+  name = 'personal assistant',
+  version = '0.2',
+  description = 'особистий асистент',
+  url = 'https://github.com/Pelmenoff/dedalus_project/tree/main/DEDALUS',
+  authors='Pelmenoff' 'Nikita-devel' 'Victor3637',
+  author_email='victorpiznak@gmail.com' 'brseven90@gmail.com' 'likhachiovvl@gmail.com' 'Titanfall@ukr.net',
+  license='MIT',
+  packages=find_namespace_packages(),
+  entry_points = ()
